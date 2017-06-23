a580237caf81ae5169f98051eb58d503076bbbef
Aleksander Rękawek <aleksander.rekawek@gmail.com>
2017-06-23 17:13:15 +0200

    Zadanie 6

--- src/main/java/bean/MockBookService.java
+++ src/main/java/bean/MockBookService.java
@@ -2,6 +2,7 @@ package bean;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.stereotype.Component;
 
@@ -11,6 +12,7 @@ import model.Book;
 public class MockBookService {
 
 	private List<Book> list;
+	private Long nextId;
 
 	public MockBookService() {
 
@@ -19,7 +21,7 @@ public class MockBookService {
 		list.add(new Book(1L, "9788324631766", "Thiniking in Java", "Bruce Eckel", "Helion", "programming"));
 		list.add(new Book(2L, "9788324627738", "Rusz glowa Java.", "Sierra Kathy, Bates Bert", "Helion", "programming"));
 		list.add(new Book(3L, "9780130819338", "Java 2. Podstawy", "Cay Horstmann, Gary Cornell", "Helion",	"programming"));
-
+		this.nextId = 4l;
 	}
 
 	public List<Book> getList() {
@@ -30,7 +32,21 @@ public class MockBookService {
 		this.list = list;
 	}
 	
-//	public Book getBookById(Long id){
-//		
-//	}
+	public Book getById(Long id){
+		Optional<Book> result = list.stream()
+				.filter(item -> item.getId()==id)
+				.findFirst();
+		
+		return result.get();
+	}
+	public void add(Book book){
+		book.setId(this.nextId++);
+		this.list.add(book);
+	}
+
+	public void deleteById(Long id) {
+		Book book = getById(id);
+		this.list.remove(book);
+		
+	}
 }
--- src/main/java/controller/BookController.java
+++ src/main/java/controller/BookController.java
@@ -1,10 +1,20 @@
 package controller;
 
+import java.io.IOException;
 import java.util.List;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 import bean.MockBookService;
 import model.Author;
@@ -13,29 +23,53 @@ import model.Book;
 @RestController
 @RequestMapping("/books")
 public class BookController {
-	
+
 	@Autowired
 	private MockBookService bookService;
-	
+
 	@RequestMapping("/hello")
 	public String hello() {
 		return "{hello: World}";
 
 	}
-	
-	
+
 	@RequestMapping("/helloBook")
 	public Book helloBook() {
 		return new Book(1L, "9788324631766", "Thiniking in Java", "Bruce Eckel", "Helion", "programming");
 	}
-	
+
 	@RequestMapping("/helloAuthor")
-	public Author helloAuthor(){
+	public Author helloAuthor() {
 		return new Author("Janusz", "Biznesu", 55);
 	}
-	
+
 	@RequestMapping("/all")
-	public List<Book> getAllBooks(){
-		return bookService.getList();
+	public List<Book> getAllBooks() {
+		List<Book> list = this.bookService.getList();
+		return list;
+	}
+
+	@GetMapping("/{id}")
+	public Book getBookFromId(@PathVariable Long id) {
+		return this.bookService.getById(id);
+	}
+
+	@PostMapping("/")
+	public void putBook(HttpServletRequest request) {
+		 
+		try {
+			String bookJson = request.getReader().readLine();
+			Book book = new ObjectMapper().readValue(bookJson, Book.class);
+			this.bookService.add(book);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+	}
+	
+	@DeleteMapping("/{id}")
+	public void deleteBook(@PathVariable Long id){
+		this.bookService.deleteById(id);
 	}
 }
